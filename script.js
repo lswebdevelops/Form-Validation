f7bc5659e45bf6e958ccb10a451c754cb5e9061c
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-01-16 21:35:54 -0300

    Changing the default message of an error when email is missing from a from using javascript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hi');
+const email = document.getElementById('mail');
+// setCustomValidity(message): Adds a custom error message to 
+// the element; if you set a custom error message, the element
+//  is considered to be invalid, and the specified error is displayed.
+//   This lets you use JavaScript code to establish a validation failure 
+//   other than those offered by the standard HTML validation constraints. 
+//   The message is shown to the user when reporting the problem.
+
+email.addEventListener('input', function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
